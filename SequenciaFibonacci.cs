872f21d18f5eff4c113281cdfe5c610948f9b1e6
victorlaves <87553418+victorlaves@users.noreply.github.com>
2021-07-19 17:05:11 -0300

    Sequencia Fibonacci
    Exercicio Fibonacci

---
+++ SequenciaFibonacci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sequencia.Fibonacci
+{
+    class SequenciaFibonacci
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+
+            Console.Write("Quantos valores : ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sequencia de Fibonacci com " + valores + " valores ");
+
+            for (int i = 0; i < valores; i++)
+            {
+                if (i == valores - 1)
+                {
+                    Console.Write(a + ".");
+                }
+                else
+                {
+                    Console.Write(a + ", ");
+                }
+
+                c = a + b;
+                a = b;
+                b = c;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
